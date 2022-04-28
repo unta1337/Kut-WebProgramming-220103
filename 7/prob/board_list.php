6a51a788c908ef9075e8ff11518b0c1ed15313f7
unta1337 <firstapple42@gmail.com>
2022-04-28 13:33:03 +0900

    7. 연습문제 완료

---
+++ 7/prob/board_list.php
@@ -0,0 +1,10 @@
+<body>
+    <?php
+        $table = $_GET["table"];
+        if ($table == "free")
+            $board_title = "자유게시판";
+        else
+            $board_title = "공지사항";
+        ?>
+    <h1><?= $board_title ?></h1>
+</body>
---
+++ 7/prob/view/view2.php
@@ -0,0 +1,10 @@
+<body>
+    <?php
+        $id = $_POST["id"];
+        $pass = $_POST["pass"];
+    ?>
+    <ul>
+        <li>아 &nbsp;이 &nbsp;디: <?= $id ?></li>
+        <li>비밀번호: <?= $pass ?></li>
+    </ul>
+</body>
